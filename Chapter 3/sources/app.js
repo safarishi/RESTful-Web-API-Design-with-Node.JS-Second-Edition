c010c962b06716286c5d641b2e6bbabb3b72c68a
safarishi <sshflgm@gmail.com>
2016-07-03 17:21:10 +0800

    add xml data for restful api

--- Chapter 3/sources/app.js
+++ Chapter 3/sources/app.js
@@ -10,38 +10,75 @@ var url = require('url');
 
 var app = express();
 
-app.get('/contacts',
-		function(request, response){
-			var get_params = url.parse(request.url, true).query;
+app.get('/contacts', (req, res) => {
+			var get_params = url.parse(req.url, true).query;
 
-			if (Object.keys(get_params).length === 0)
-			{
-				response.setHeader('content-type', 'application/json');
-				response.end(JSON.stringify(contacts.list()));
+			var output;
+
+			res.format({
+				'application/json': () => {
+					output = JSON.stringify(contacts.list());
+					theOtherOuput = JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value));
+				},
+				'text/xml': () => {
+					output = contacts.listXml();
+					theOtherOuput = contacts.queryByArgXml(get_params.arg, get_params.value);
+				},
+				default: () => {
+					res.status(406).send('Not Acceptable');
+				}
+			})
+
+			if (Object.keys(get_params).length === 0) {
+				res.end(output);
 			}
-			else
-			{
-				response.setHeader('content-type', 'application/json');
-				response.end(JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value)));
+			else {
+				res.end(theOtherOuput)
 			}
 		}
 );
 
-app.get('/contacts/:number', function(request, response) {
-	response.setHeader('content-type', 'application/json');
-	response.end(JSON.stringify(contacts.query(request.params.number)));
+app.get('/contacts/:number', function(req, res) {
+	res.format({
+		'application/json': () => {
+			res.end(JSON.stringify(contacts.query(req.params.number)));
+		},
+		'text/xml': () => {
+			res.end(contacts.queryXml(req.params.number));
+		},
+		default: () => {
+			res.status(406).end('Not Acceptable');
+		}
+	});
 });
 
-app.get('/groups', function(request, response) {
-	console.log ('groups');
-	response.setHeader('content-type', 'application/json');
-	response.end(JSON.stringify(contacts.list_groups()));
+app.get('/groups', (req, res) => {
+	res.format({
+		default: () => {
+			res.status(406).send('Not Acceptable');
+		},
+		'application/json': () => {
+			res.send(JSON.stringify(contacts.list_groups()));
+		},
+		'text/xml': () => {
+			res.send(contacts.listGroupXml());
+		},
+	});
 });
 
-app.get('/groups/:name', function(request, response) {
-	console.log ('groups');
-	response.setHeader('content-type', 'application/json');
-	response.end(JSON.stringify(contacts.get_members(request.params.name)));
+app.get('/groups/:name', function(req, res) {
+	var name = req.params.name;
+	res.format({
+		'application/json': () => {
+			res.end(JSON.stringify(contacts.get_members(name)));
+		},
+		'text/xml': () => {
+			res.end(contacts.getXmlMember(name))
+		},
+		default: () => {
+			res.status(406).end('Not Acceptable');
+		}
+	})
 });
 
 http.createServer(app).listen(3000, function(){
--- Chapter 3/sources/modules/contacts.js
+++ Chapter 3/sources/modules/contacts.js
@@ -1,89 +1,128 @@
 var fs = require('fs');
+var js2xmlparser = require("js2xmlparser");
 
 function read_json_file()
 {
-	var file = './data/contacts.json';	
-	return fs.readFileSync(file);
+  var file = './data/contacts.json';
+  return fs.readFileSync(file);
 }
 
 exports.list = function()
-{	
-	return JSON.parse(read_json_file());	
+{
+  console.log('list');
+  return JSON.parse(read_json_file());
 };
 
+exports.listXml = () => {
+  var resObj = this.list();
+
+  return js2xmlparser('rs', resObj);
+}
+
 exports.query = function(number)
-{	
-	var json = read_json_file();
-	var json_result = JSON.parse(json);
-	var result = json_result.result;
-	
-	for (var i = 0; i < result.length; i++)
-	{
-		var contact = result[i];
-		if (contact.primarycontactnumber === number)
-		{
-			return contact;
-		}
-	}
-	return null;
+{
+  var json = read_json_file();
+  var json_result = JSON.parse(json);
+  var result = json_result.result;
+
+  for (var i = 0; i < result.length; i++)
+  {
+    var contact = result[i];
+    if (contact.primarycontactnumber === number)
+    {
+      return contact;
+    }
+  }
+  return null;
 };
+
+exports.queryXml = (number) => {
+  var obj = this.query(number);
+  if (obj === null) {
+    return js2xmlparser('rs', 'null');
+  }
+
+  return js2xmlparser('rs', obj);
+}
 
 exports.query_by_arg = function(arg, value)
 {
-	var json = read_json_file();
-	var json_result = JSON.parse(json);
-	var result = json_result.result;
-	
-	for (var i = 0; i < result.length; i++)
-	{
-		var contact = result[i];
-		if (contact[arg] === value)
-		{
-			return contact;
-		}
-	}
-	return null;
-};	
-	
+  var json = read_json_file();
+  var json_result = JSON.parse(json);
+  var result = json_result.result;
+
+  for (var i = 0; i < result.length; i++)
+  {
+    var contact = result[i];
+    if (contact[arg] === value)
+    {
+      return contact;
+    }
+  }
+  return null;
+};
+
+exports.queryByArgXml = (arg, value) => {
+  var obj = this.query_by_arg(arg, value);
+
+  if (obj === null) {
+    return js2xmlparser('rs', 'null');
+  }
+
+  return js2xmlparser('rs', obj);
+}
+
 exports.list_groups = function()
 {
-	var json = read_json_file();
-	var json_result = JSON.parse(json);
-	var result = json_result.result;
-	
-	var resultArray = [];
-	
-	for (var i = 0; i < result.length; i++)
-	{
-		var groups = result[i].groups;
-		for (var index = 0; index < groups.length; index++)
-		{
-			if (resultArray.indexOf(groups[index]) ===-1)
-			{
-				resultArray.push(groups[index]);
-			}
-		}
-	}
-	
-	return resultArray;
+  var json = read_json_file();
+  var json_result = JSON.parse(json);
+  var result = json_result.result;
+
+  var resultArray = [];
+
+  for (var i = 0; i < result.length; i++)
+  {
+    var groups = result[i].groups;
+    for (var index = 0; index < groups.length; index++)
+    {
+      if (resultArray.indexOf(groups[index]) ===-1)
+      {
+        resultArray.push(groups[index]);
+      }
+    }
+  }
+
+  return resultArray;
 };
+
+exports.listGroupXml = () => {
+  var obj = this.list_groups();
+
+  return js2xmlparser('groups', {group: obj});
+}
 
 exports.get_members = function(group_name)
 {
-	var json = read_json_file();
-	var json_result = JSON.parse(json);
-	var result = json_result.result;
-	
-	var resultArray = [];
-	
-	for (var i = 0; i < result.length; i++)
-	{	
-		
-		if (result[i].groups.indexOf(group_name) > -1)
-		{
-			resultArray.push(result[i]);
-		}
-	}
-	
-	return resultArray;
+  var json = read_json_file();
+  var json_result = JSON.parse(json);
+  var result = json_result.result;
+
+  var resultArray = [];
+
+  for (var i = 0; i < result.length; i++)
+  {
+
+    if (result[i].groups.indexOf(group_name) > -1)
+    {
+      resultArray.push(result[i]);
+    }
+  }
+
+  return resultArray;
 };
+
+exports.getXmlMember = (groupName) => {
+  var arr = this.get_members(groupName);
+
+  return js2xmlparser('rs', { member: arr });
+}
