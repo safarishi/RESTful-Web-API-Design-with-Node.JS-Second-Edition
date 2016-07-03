698bb2c66377f21ec6d281feb2efcb4bed7703bb
safarishi <sshflgm@gmail.com>
2016-07-03 15:09:23 +0800

    add .gitignore remove useless blank

--- Chapter 2/test-http-module.js
+++ Chapter 2/test-http-module.js
@@ -1,24 +1,40 @@
 var sinon = require('sinon');
 
-exports.test_handle_GET_request = function (test) {
-	
-	var response = {'writeHead' : function () {}, 'end': function() {}};
-	var responseMock = sinon.mock(response);      
-    
-    responseMock.expects('end').once().withArgs('Get action was requested');
+exports.test_handle_GET_request = (test) => {
+
+	var response = {writeHead: () => {}, end: () => {}};
+	var responseMock = sinon.mock(response);
+
     responseMock.expects('writeHead').once().withArgs(200, {
-		'Content-Type' : 'text/plain'
-	});
-    
+        'Content-Type' : 'text/plain'
+    });
+    responseMock.expects('end').once().withArgs('Get action was requested');
+
     var request = {};
     var requestMock = sinon.mock(request);
     requestMock.method = 'GET';
-    
-    var http_module = require('../modules/http-module');
-    
+
+    var http_module = require('./modules/http-module');
+
     http_module.handle_request(requestMock, response);
-    
+
     responseMock.verify();
     test.done();
 };
 
+
+exports.testDemo = (test) => {
+    var api = {
+        methodY: (arg) => {
+            return arg;
+        }
+    }
+
+    var mock = sinon.mock(api);
+
+    mock.expects('methodY').once().withArgs('xyz').returns('xyz');
+
+    test.equals(api.methodY('xyz'), 'xyz');
+
+    test.done();
+}
--- Chapter 3/sources/app.js
+++ Chapter 3/sources/app.js
@@ -1,8 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var path = require('path');
 
@@ -15,14 +10,14 @@ var url = require('url');
 
 var app = express();
 
-app.get('/contacts', 
+app.get('/contacts',
 		function(request, response){
-			var get_params = url.parse(request.url, true).query;	
-			
+			var get_params = url.parse(request.url, true).query;
+
 			if (Object.keys(get_params).length === 0)
 			{
 				response.setHeader('content-type', 'application/json');
-				response.end(JSON.stringify(contacts.list()));	
+				response.end(JSON.stringify(contacts.list()));
 			}
 			else
 			{
@@ -32,27 +27,23 @@ app.get('/contacts',
 		}
 );
 
-
 app.get('/contacts/:number', function(request, response) {
-	response.setHeader('content-type', 'application/json');	
+	response.setHeader('content-type', 'application/json');
 	response.end(JSON.stringify(contacts.query(request.params.number)));
 });
 
 app.get('/groups', function(request, response) {
 	console.log ('groups');
-	response.setHeader('content-type', 'application/json');	
+	response.setHeader('content-type', 'application/json');
 	response.end(JSON.stringify(contacts.list_groups()));
 });
 
 app.get('/groups/:name', function(request, response) {
 	console.log ('groups');
-	response.setHeader('content-type', 'application/json');	
+	response.setHeader('content-type', 'application/json');
 	response.end(JSON.stringify(contacts.get_members(request.params.name)));
 });
-	
-
-
 
 http.createServer(app).listen(3000, function(){
-  console.log('Express server listening on port 3000');  
+  console.log('Express server listening on port 3000');
 });
